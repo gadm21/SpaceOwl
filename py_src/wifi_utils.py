8d1006bffb3005063c021bcf2dc97a472f867ee9
gadm21 <g.gad@nu.edu.eg>
2024-02-28 21:33:27 -0500

    updates

---
+++ py_src/wifi_utils.py
@@ -0,0 +1,41 @@
+import subprocess
+
+def what_wifi():
+    process = subprocess.run(['nmcli', '-t', '-f', 'ACTIVE,SSID', 'dev', 'wifi'], stdout=subprocess.PIPE)
+    if process.returncode == 0:
+        return process.stdout.decode('utf-8').strip().split(':')[1]
+    else:
+        return ''
+
+def is_connected_to(ssid: str):
+    return what_wifi() == ssid
+
+def scan_wifi():
+    process = subprocess.run(['nmcli', '-t', '-f', 'SSID,SECURITY,SIGNAL', 'dev', 'wifi'], stdout=subprocess.PIPE)
+    if process.returncode == 0:
+        return process.stdout.decode('utf-8').strip().split('\n')
+    else:
+        return []
+        
+def is_wifi_available(ssid: str):
+    return ssid in [x.split(':')[0] for x in scan_wifi()]
+
+def connect_to(ssid: str, password: str):
+    if not is_wifi_available(ssid):
+        return False
+    subprocess.call(['nmcli', 'd', 'wifi', 'connect', ssid, 'password', password])
+    return is_connected_to(ssid)
+
+def connect_to_saved(ssid: str):
+    if not is_wifi_available(ssid):
+        return False
+    subprocess.call(['nmcli', 'c', 'up', ssid])
+    return is_connected_to(ssid)
+
+def disconnect():
+    subprocess.call(['nmcli', 'd', 'disconnect'])
+
+if __name__ == "__main__":
+    print(what_wifi())
+    print(scan_wifi())
+    print(is_connected_to('BTHub5-2G8B'))
